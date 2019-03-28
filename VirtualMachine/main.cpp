97991737cd952793736c2bce15751ce331d75e9b
Zinenko Dmytro <carakanz@gmail.com>
2019-03-29 00:25:25 +0300

    add main

--- VirtualMachine/main.cpp
+++ VirtualMachine/main.cpp
@@ -1,9 +1,27 @@
 #pragma once
 #include "Run.h"
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "hello" << std::endl;
+    std::ifstream in(argv[1], std::ios::binary);
+    std::unique_ptr<VMState> state = std::make_unique<VMState>();
+    in >> state->rax >>
+        state->rbx >>
+        state->rcx >>
+        state->rdx >>
+        state->rsp >>
+        state->rbp >>
+        state->rip >>
+        state->interruptStart;
+    uint64_t size;
+    in >> size;
+    state->memory.resize(size);
+    in.read(reinterpret_cast<char*>(state->memory.data()), size);
+
+    CRun runner;
+    runner.Load(std::move(state));
+    runner.Start();
     return 0;
 }
